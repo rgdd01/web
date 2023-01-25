30c3142e9ba19f88187c45a43dc76a491f5c11c7
raghadalothman <raghad.alo1@hotmail.com>
2023-01-25 23:37:24 +0300

    new

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+function Confirm(){
+
+   // window.alert("your reservation has been successfully confirmed");
+     document.getElementById('Confirm').innerHTML = 'your reservation has been successfully confirmed';
+
+
+}
