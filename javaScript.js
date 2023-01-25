abd6d39e51e6728c4d4a7fc4013448d4b7fa0c56
raghadalothman <raghad.alo1@hotmail.com>
2023-01-26 01:48:23 +0300

    updates

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,15 @@
 function Confirm(){
-
-   // window.alert("your reservation has been successfully confirmed");
-     document.getElementById('Confirm').innerHTML = 'your reservation has been successfully confirmed';
+  document.getElementById('Confirm').innerHTML = 'your reservation has been successfully confirmed';
 
 }
 function age(){
   var ud = document.getElementById('day').value;
   var um =document.getElementById('month').value;
-  var uy = document.getElementById('year').value;
   var date = new Date();
-   var d = date.getDay();
-   var m =1+ date.getMonth();
-   var y = date.getFullYear();
-   var 	Month = [31, 28,31,30,31,30,31,31,30,31,30,31];
-      if (ud >  d){
-          d =  d+ Month[ m-1];
-          m =  m - 1;
-          }
-          if (um >  m){
-          m =  m+ 12;
-          y = y -1;
-          }
-      //var Rd= d-ud;
-      //var Rm= m-um;
+  var d = date.getDate();
+  var m =1+ date.getMonth();
+  var 	Month = [31, 28,31,30,31,30,31,31,30,31,30,31];
+     
       if(ud==d && um==m){
         document.getElementById('BDc').innerHTML='You got the discount !';
       }
@@ -32,5 +19,5 @@ function age(){
 }  
 
 
-}
+
 
