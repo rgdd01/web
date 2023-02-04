b99200ef9942078f8628ad4a6131c99aaaa3ce65
haya60 <hayaalrusais@gmail.com>
2023-02-04 15:48:01 +0300

    modify sizes

--- javaScript.js
+++ javaScript.js
@@ -18,9 +18,13 @@ function age(){
       }
 } 
 
+// to get the date og today
 function getDate(){
-    var today = new Date();
+  var today = new Date();
+
 document.getElementById("date").value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+
+
 }
 
 function Login(){
