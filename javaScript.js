805f05b98d1d92143e129418a414b713ed78b0a4
raghadalothman <raghad.alo1@hotmail.com>
2023-02-10 15:56:07 +0300

    updates

--- javaScript.js
+++ javaScript.js
@@ -28,20 +28,9 @@ document.getElementById("start").value = today.getFullYear() + '-' + ('0' + (tod
 
 }
 
-function Login(){
-  var name = document.getElementById('loginname').value;
-  var passowrd = document.getElementById('loginpassword').value;
-   // عشان نتاكد من البيانات اللي داخله لازم نصير مشتغلين على الداتابيس
-  
-}
 
 function Signup(){
-  var name = document.getElementById('sname').value;
-  var passowrd = document.getElementById('spassword').value;
-  var email = document.getElementById('semail').value;
-
-  //مفروض تتسجل هذي البيانات في الداتابيس 
-
+ 
   // ظهور نافذة التاكيد (هل انت متاكد)
   return confirm("Are you sure it is your correct email?");
 
