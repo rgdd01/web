7f1e867b5550be7b73bd26a7868628107c0546c6
raghadalothman <raghad.alo1@hotmail.com>
2023-02-11 20:31:53 +0300

    final updates1

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,5 @@
 function Confirm(){
-  document.getElementById('Confirm').innerHTML = 'Your reservation has been successfully confirmed ! Thank you.';
+  document.getElementById('Confirm_msg').innerHTML = 'Your reservation has been successfully confirmed ! Thank you.';
 }
 
 function age(){
@@ -9,24 +9,22 @@ function age(){
   var d = date.getDate();
   var m =1+ date.getMonth();
       if(ud==d && um==m){
-        document.getElementById('BDc').innerHTML='You got the discount !';
+        document.getElementById('message').innerHTML='You got the discount !';
       }
       else{
-        document.getElementById('BDc').innerHTML='Sorry , today is not your birthday !';
+        document.getElementById('message').innerHTML='Sorry , today is not your birthday !';
       }
 } 
-//var a = document.querySelector('#start');
-//var date =new Date();
-//a.value=+date.getFullYear().toString()+'-'+(date.getMonth()+1).toString().padStart(2,0)+'-'+date.getDate().toString().padStart(2,0);
 
 // to get the date og today
-function getDate(){
+function getdate(){
   var today = new Date();
 
 document.getElementById("start").value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
 
 
 }
+window.addEventListener('load',getdate, false);
 
 
 function Signup(){
